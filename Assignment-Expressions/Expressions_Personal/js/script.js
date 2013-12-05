492348846ad1a3dc952948e6907cc4515dd51d5a
joeyfuller <joeyfuller@fullsail.edu>
2013-12-05 15:12:20 -0500

    updating my work after starting the personal expression

--- Assignment-Expressions/Expressions_Personal/js/script.js
+++ Assignment-Expressions/Expressions_Personal/js/script.js
@@ -1,1 +1,7 @@
-// JavaScript Document
+// Expressions Personal
+//Joey Fuller
+//12-5-13
+
+//on average how many sodas were drank in a weeks time
+
+alert("Hello, lets see how many sodas you drink on an average day!");
