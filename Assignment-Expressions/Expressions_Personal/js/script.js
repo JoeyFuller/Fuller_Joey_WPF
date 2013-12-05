3b03c948bea5d2d18cc55aa36bd375c4be50af5f
joeyfuller <joeyfuller@fullsail.edu>
2013-12-05 16:13:49 -0500

    Making progress on the wacky section

--- Assignment-Expressions/Expressions_Personal/js/script.js
+++ Assignment-Expressions/Expressions_Personal/js/script.js
@@ -5,6 +5,7 @@
 //on average how many sodas were drank in a weeks time
 
 alert("Hello, lets see how many sodas you drink on an average day!");
+
 var input1 = prompt("How many sodas did you drink on day 1?");//start of user input
 var input2 = prompt("How many sodas did you drink on day 2?");
 var input3 = prompt("How many sodas did you drink on day 3?");
@@ -12,8 +13,11 @@ var input4 = prompt("How many sodas did you drink on day 4?");
 var input5 = prompt("How many sodas did you drink on day 5?");
 var input6 = prompt("How many sodas did you drink on day 6?");
 var input7 = prompt("How many sodas did you drink on day 7?");//end of user input
+
 var days = [input1, input2, input3, input4, input5, input6, input7];//array for user input average
+
 var total = days[0]+days[1]+days[2]+days[3]+days[4]+days[5]+days[6];
+
 var averageSodas = total/7;
 
 var sodasPerWeek = total;
--- Assignment-Expressions/Expressions_Wacky/js/script.js
+++ Assignment-Expressions/Expressions_Wacky/js/script.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+// Expressions Wacky
+//Joey Fuller
+//12-5-13
+//find the correct numbers!
+alert("Enter five numbers and try to find the correct pattern to make the total equal");
+
+var first = prompt("Enter the first number");
+var second = prompt("Enter the second number");
+var third = prompt("Enter the third number");
+var forth = prompt("Enter the forth number");
+var fifth = prompt("Enter the final number!");
+var total = 1 * second * fifth + third / forth - first;
+
+alert("Your total is " + total);
