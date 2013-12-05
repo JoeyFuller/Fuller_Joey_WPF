9cb517be7a8cc2afef65a25e845e57d5e063a5de
joeyfuller <joeyfuller@fullsail.edu>
2013-12-05 15:52:51 -0500

    Attempting to place user inputs in an array with no luck, may have to change my plan

--- Assignment-Expressions/Expressions_Personal/js/script.js
+++ Assignment-Expressions/Expressions_Personal/js/script.js
@@ -5,11 +5,17 @@
 //on average how many sodas were drank in a weeks time
 
 alert("Hello, lets see how many sodas you drink on an average day!");
-var day1 = prompt("How many sodas did you drink?");//start of user input
-var day2 = prompt("How many sodas did you drink?");
-var day3 = prompt("How many sodas did you drink?");
-var day4 = prompt("How many sodas did you drink?");
-var day5 = prompt("How many sodas did you drink?");
-var day6 = prompt("How many sodas did you drink?");
-var day7 = prompt("How many sodas did you drink?");//end of user input
-var days = [day1, day2, day3, day4, day5, day6, day7];//array for user input average
+var input1 = prompt("How many sodas did you drink on day 1?");//start of user input
+var input2 = prompt("How many sodas did you drink on day 2?");
+var input3 = prompt("How many sodas did you drink on day 3?");
+var input4 = prompt("How many sodas did you drink on day 4?");
+var input5 = prompt("How many sodas did you drink on day 5?");
+var input6 = prompt("How many sodas did you drink on day 6?");
+var input7 = prompt("How many sodas did you drink on day 7?");//end of user input
+var days = [input1, input2, input3, input4, input5, input6, input7];//array for user input average
+var total = days[0]+days[1]+days[2]+days[3]+days[4]+days[5]+days[6];
+var averageSodas = total/7;
+
+var sodasPerWeek = total;
+
+alert("You drink " + averageSodas + " per day on average! " + "That is " + sodasPerWeek + " per week!");
