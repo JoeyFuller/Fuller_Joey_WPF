293c0eb7d7806eb04abfe9cc287c4ec5fa4af45d
joeyfuller <joeyfuller@fullsail.edu>
2013-12-05 15:20:10 -0500

    update after adding array in personal js

--- Assignment-Expressions/Expressions_Personal/js/script.js
+++ Assignment-Expressions/Expressions_Personal/js/script.js
@@ -5,3 +5,11 @@
 //on average how many sodas were drank in a weeks time
 
 alert("Hello, lets see how many sodas you drink on an average day!");
+var day1 = prompt("How many sodas did you drink?");//start of user input
+var day2 = prompt("How many sodas did you drink?");
+var day3 = prompt("How many sodas did you drink?");
+var day4 = prompt("How many sodas did you drink?");
+var day5 = prompt("How many sodas did you drink?");
+var day6 = prompt("How many sodas did you drink?");
+var day7 = prompt("How many sodas did you drink?");//end of user input
+var days = [day1, day2, day3, day4, day5, day6, day7];//array for user input average
