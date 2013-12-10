ce51b63830e8abbaa6cf9635f40e17e50787d158
joeyfuller <joeyfuller@fullsail.edu>
2013-12-09 19:50:18 -0500

    Added a few assignments that are for practice and follow along with the lesson for the week

---
+++ If and else/js/script.js
@@ -0,0 +1,14 @@
+// Joey Fuller
+
+var kidHeight = 52;
+var minHeight = 48;
+
+//if the child is old enough, print to console "you can ride!"
+//if the kid is over 48 inches of height
+if(kidHeight > minHeight){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}else{
+	//code performed is condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
+}
