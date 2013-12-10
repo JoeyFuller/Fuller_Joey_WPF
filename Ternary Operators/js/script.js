38be7e7b4562207c2921d8d0a2252e51f9090d59
joeyfuller <joeyfuller@fullsail.edu>
2013-12-09 20:10:35 -0500

    Attempt to fix the file system

--- Ternary Operators/js/script.js
+++
@@ -1,8 +0,0 @@
-// Joey Fuller
-
-var age = 6;
-var book;
-
-//if the child is under 10, they get "Green Eggs and Ham". Otherwise they get "Time Machine"
-book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
-console.log(book);
