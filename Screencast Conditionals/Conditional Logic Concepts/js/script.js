aa9442e7229e4465bbab1c165a0fa4ddd4cdcdde
joeyfuller <joeyfuller@fullsail.edu>
2013-12-09 20:12:01 -0500

    Fixed the upload file system

---
+++ Screencast Conditionals/Conditional Logic Concepts/js/script.js
@@ -0,0 +1,9 @@
+//Joey Fuller
+var goToBeach
+var goToMovie
+
+if(sunny){
+	goToBeach();
+}else{
+	goToMovie();
+}
---
+++ Screencast Conditionals/Nesting Conditionals/js/script.js
@@ -0,0 +1,14 @@
+// Joey Fuller
+var goToBeach
+var goToMovie
+var wearNewSwimSuit
+var warmWater
+
+if(sunny){
+	goToBeach();
+	if(warmWater){
+		wearNewSwimSuit();
+	}
+}else{
+	goToMovie();
+}
