597430e109922f6f11e019cdb3855e4a97c75c40
joeyfuller <joeyfuller@fullsail.edu>
2013-12-12 07:23:43 -0500

    Added alert into the system rather than just printing to console.log, however I left the console.log in the code for testing. Also added a end note for input of services needed and will continue on to pricing.

--- Assignment Conditionals/Industry/js/script.js
+++ Assignment Conditionals/Industry/js/script.js
@@ -6,12 +6,19 @@ var inputHosting = prompt("Do you need a server to host it on? (Y or N)")
 
 if(inputWebsite === "Y" && inputHosting === "Y"){
 	console.log("We see you need a website and hosting!");
+	alert("We see you need a website and hosting!");
 }else if(inputWebsite === "Y" && inputHosting === "N"){
 	console.log("We see you only need a website!");
+	alert("We see you only need a website!");
 }else if(inputWebsite === "N" && inputHosting === "Y"){
 	console.log("We see you only need hosting!");
+	alert("We see you only need hosting!");
 }else if(inputWebsite === "N" && inputHosting === "N"){
 	console.log("We see that you are unsure of your choice, please contact us for more information!");
+	alert("We see that you are unsure of your choice, please contact us for more information!");
 }else{
 	console.log("You choices of " + inputWebsite + " and " + inputHosting + " are unclear, please try again using the approved Y for yes and N for no.");
+	alert("You choices of " + inputWebsite + " and " + inputHosting + " are unclear, please try again using the approved Y for yes and N for no.");
 }
+//End of finding out what services they need and start of pricing!
+
