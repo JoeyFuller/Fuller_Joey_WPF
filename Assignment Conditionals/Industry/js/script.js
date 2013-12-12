b479302bdf45328768ba02ebaa667cc873dbbbe4
joeyfuller <joeyfuller@fullsail.edu>
2013-12-12 07:17:16 -0500

    Fixed the string issue with the input.
    The input was seen as a variable rather than a string. Adding " " on variable fixed the issue.

--- Assignment Conditionals/Industry/js/script.js
+++ Assignment Conditionals/Industry/js/script.js
@@ -4,13 +4,13 @@
 var inputWebsite = prompt("Do you need a website? (Y or N)")
 var inputHosting = prompt("Do you need a server to host it on? (Y or N)")
 
-if(inputWebsite === Y && inputHosting === Y){
+if(inputWebsite === "Y" && inputHosting === "Y"){
 	console.log("We see you need a website and hosting!");
-}else if(inputWebsite === Y && inputHosting === N){
+}else if(inputWebsite === "Y" && inputHosting === "N"){
 	console.log("We see you only need a website!");
-}else if(inputWebsite === N && inputHosting === Y){
+}else if(inputWebsite === "N" && inputHosting === "Y"){
 	console.log("We see you only need hosting!");
-}else if(inputWebsite != Y && inputHosting != Y){
+}else if(inputWebsite != "Y" && inputHosting != "Y"){
 	console.log("We see that you are unsure of your choice, please contact us for more information!");
 }else{
 	console.log("You choices of " + inputWebsite + " and " + inputHosting + " we unclear, please try again!");
