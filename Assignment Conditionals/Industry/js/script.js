c5b0b6dc82b69817c3cca0fff3fd93c77ada1e90
joeyfuller <joeyfuller@fullsail.edu>
2013-12-12 07:14:54 -0500

    Work progress on the industry section of the assignment

--- Assignment Conditionals/Industry/js/script.js
+++ Assignment Conditionals/Industry/js/script.js
@@ -1,1 +1,17 @@
-// JavaScript Document
+// Joey Fuller
+// Industry Conditionals
+
+var inputWebsite = prompt("Do you need a website? (Y or N)")
+var inputHosting = prompt("Do you need a server to host it on? (Y or N)")
+
+if(inputWebsite === Y && inputHosting === Y){
+	console.log("We see you need a website and hosting!");
+}else if(inputWebsite === Y && inputHosting === N){
+	console.log("We see you only need a website!");
+}else if(inputWebsite === N && inputHosting === Y){
+	console.log("We see you only need hosting!");
+}else if(inputWebsite != Y && inputHosting != Y){
+	console.log("We see that you are unsure of your choice, please contact us for more information!");
+}else{
+	console.log("You choices of " + inputWebsite + " and " + inputHosting + " we unclear, please try again!");
+}
