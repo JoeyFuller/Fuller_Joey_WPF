41c72c67fe077450a91339512d5f09ffb7ff65f5
joeyfuller <joeyfuller@fullsail.edu>
2013-12-19 04:44:50 -0500

    Having a hard time inputing the variable into the function, unsure as of why it is not working

--- Assignment Functions/Functions_Industry/js/script.js
+++ Assignment Functions/Functions_Industry/js/script.js
@@ -2,31 +2,37 @@
 // 12 - 19 - 13
 // Functions_Industry
 
-var name = prompt("Hello what is your name?");
+var name = prompt("Hello what is your name?"); //a prompt to ask your name for various outputs thoughtout the code
 alert("Great! Welcome " + name + " lets see how long it will take for you to personally complete a website!");
 
-var experiance = prompt("Do you have any experiance with web coding?(css, html, javaScript) (Y or N)");
+var experiance = prompt("Do you have any experiance with web coding?(css, html, javaScript) (Y or N)"); //input to check for experiance
 
-var gpu = prompt("Do you have the graphics laid out? (Y or N)")
+var gpu = prompt("Do you have the graphics laid out? (Y or N)") // input to see if you have graphics picked
 
-if(experiance === "Y" && gpu === "Y"){
+if(experiance === "Y" && gpu === "Y"){  //output for the variables experiance and gpu
 	console.log("Great you seem to be ahead of the curve!");
 	alert("Great you seem to be ahead of the curve!");
 }else if(experiance === "y" && gpu === "y"){
 	console.log("Great you seem to be ahead of the curve!");
 	alert("Great you seem to be ahead of the curve!");
+}else if(experiance === "Y" && gpu === "y"){
+	console.log("Great you seem to be ahead of the curve!");
+	alert("Great you seem to be ahead of the curve!");
+}else if(experiance === "y" && gpu === "Y"){
+	console.log("Great you seem to be ahead of the curve!");
+	alert("Great you seem to be ahead of the curve!");
 }else{
 	console.log("You seem to be new at this, but lets continue!");
 	alert("You seem to be new at this, but lets continue!");
-}
+}  // end of output for variables experiance and gpu
 
-var numberOfPages = prompt("How many pages do you plan on making?");
+var numberOfPages = prompt("How many pages do you plan on making?"); // input for the amount of pages on the website
 	
-var totalHours = function hoursPerPage(numberOfPages){
+var totalHours = function(numberOfPages){
 	var hours = 4 * numberOfPages;
 	return hours;
 }
-
-console.log(totalHours);
-alert(totalHours);
+var userPages = totaHours(numberOfPages);
+console.log(userPages);
+alert(userPages);
 
