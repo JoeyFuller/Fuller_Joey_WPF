452c5216d20972642c73d534602aea9e42332363
joeyfuller <joeyfuller@fullsail.edu>
2013-12-19 03:13:49 -0500

    Finished the first problem moving on to the second

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -3,13 +3,13 @@
 // Functions_worksheet
 
 // Circumference
-var circleCircumference = circleRadius(10);
+var circleCircumference = circleRadius(10); // this is the return variable or where the return reports too using a anon function
 
-function circleRadius(radius){
+function circleRadius(radius){  //thi is the function that does the calculations for the circumference
 	var circumference = 2 * radius * Math.PI;
-	return circumference;
+	return circumference;//this is the return that spits the results back into the anon function
 }
 
-console.log("The circumference of the circle is " + circleCircumference);
+console.log("The circumference of the circle is " + circleCircumference); //simple print out
 
 
