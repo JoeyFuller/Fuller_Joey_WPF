234b998d883718bd54bb71e76ce66c93872c715a
joeyfuller <joeyfuller@fullsail.edu>
2013-12-19 03:01:39 -0500

    I have the code written but it is spitting out the actual code rather than calculations, working on fixing that.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,3 +2,14 @@
 // 12 - 19 - 13
 // Functions_worksheet
 
+// Circumference
+var circleCircumference = function circle(radius){
+	var circumference = 2 * radius * Math.PI;
+	return circle;
+}
+
+var answer = circleCircumference(10);
+
+console.log("The circumference of the circle is " + circleCircumference);
+
+
