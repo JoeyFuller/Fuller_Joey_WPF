004adb92323eb0b3504f9370953fb01a5501154d
joeyfuller <joeyfuller@fullsail.edu>
2013-12-19 06:31:41 -0500

    Added the proper story lines, coded the function properly as i had troubles before hand

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -11,10 +11,15 @@ var trait = prompt("Now we have a name, what is their main trait? (ex. Happy, br
 var year = prompt("Great! Now what year is " + character + " living in?");
 var house = prompt("Okay, we have a main character and a time, now where does " + character + " live?");
 var friend = prompt("There is always a side kick or friend! Pick one and be creative!");
-var activity = prompt("We are getting closer to the story, which is an activity. Our characters need an activity too!");
+var activity = prompt("We are getting closer to the story, which is an activity. Our characters need an activity too!(make it in the past tense");
 var end = prompt("The story is ending! Finish this sentence! ...and they lived (blank)");
 alert("Okay, now lets see what we came up with!");
 console.log("Okay, now lets see what we came up with!");
 
 var story = function storyTime(character, trait, year, house, friend, activity, end){
-	alert("Once upon a time there was a " + trait + " adventurer, whos name was " + character + " 
+	var finish = "Once upon a time in the year" + year + ", there was a " + trait + " adventurer, whos name was " + character + ". " + friend + " was " + character + "'s sidekick and they were always getting in trouble! Once they even " + activity + "! After that life went on as normal and they lived " + end + "!";
+	return finish;
+}
+var complete = story(character, trait, year, house, friend, activity, end);
+alert(complete);
+
