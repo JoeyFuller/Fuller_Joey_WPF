92094eba3cf5dd005a88d133e3d1b9cbfb257863
joeyfuller <joeyfuller@fullsail.edu>
2013-12-19 03:19:27 -0500

    Finished both sections, both print out properly

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -12,4 +12,15 @@ function circleRadius(radius){  //thi is the function that does the calculations
 
 console.log("The circumference of the circle is " + circleCircumference); //simple print out
 
+//START OF Stung!
+
+var stings = stingsPerWeight(10);
+
+function stingsPerWeight(weight){
+	var total = 8.666666667 * weight;
+	return total;
+}
+console.log("It takes " + stings + " to kill this animal.");
+
+
 
